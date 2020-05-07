da613d07fe23e558a1464d32c12b4e3eab9250bc
Prateek Joshi <prateekjoshi2013@gmail.com>
2020-05-07 19:12:17 -0400

    added correct config

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,14 +1,13 @@
 export const config = {
   "dev": {
-    "username": "myudagramdev",
-    "password": "myudagramdev",
-    "database": "myudagramdev",
-    "host": "udagramprateekdev.clgkdpybbqoy.us-east-1.rds.amazonaws.com",
+    "username": "udagrampjoshidev",
+    "password": "udagrampjoshidev",
+    "database": "udagrampjoshidev",
+    "host": "database-1.clcngps67ims.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket_region": "us-east-1",
-    "aws_media_bucket": "udagram-joshi-dev"
+    "aws_media_bucket": "udagram-pjoshi-dev"
   },
   "prod": {
     "username": "",
