a2e901cb59c12edb61adc69cf66d67311c330e23
Prateek Joshi <prateekjoshi2013@gmail.com>
2020-05-06 23:57:55 -0400

    added credentails to aws.ts

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,14 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "myudagramdev",
+    "password": "myudagramdev",
+    "database": "myudagramdev",
+    "host": "udagramprateekdev.clgkdpybbqoy.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket_region": "us-east-1",
+    "aws_media_bucket": "udagram-joshi-dev"
   },
   "prod": {
     "username": "",
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,31 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
-
+router.get('/:id', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+        //@TODO try it yourself
+        const {id}=req.params
+        if(!id)
+        return res.status(500).send("please provide a valid id")
+        const feedItem=await FeedItem.findByPk(id);
+        return res.status(200).send(feedItem)
+});
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        const {id}=req.params
+        console.log(req.body);
+        if(!id)
+        return res.status(500).send("please provide a valid id")
+    const feedItem= await FeedItem.update(req.body, {
+            where: {
+              id: id
+            }
+          });
+        res.status(200).send(feedItem)
 });
 
 
